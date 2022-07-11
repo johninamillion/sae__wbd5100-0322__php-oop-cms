ffe7479f7901e9cdd9d98038ee77e9b9bb3cc487
johninamillion <johninamio@gmail.com>
2022-07-11 10:27:09 +0200

    create Application class

---
+++ src/Application.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace SAE\PHPCMS;
+
+final class Application {
+
+    /**
+     * Run application
+     *
+     * @access  public
+     * @return  void
+     */
+    public function run() : void {
+
+        echo "Application run!";
+    }
+
+}
