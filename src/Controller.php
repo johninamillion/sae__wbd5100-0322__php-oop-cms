d9027a3358d3d1535ca01cdb2d92fc45cab53c01
johninamillion <johninamio@gmail.com>
2022-07-11 11:30:42 +0200

    create abstract Controller class

---
+++ src/Controller.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace SAE\PHPCMS;
+
+abstract class Controller {
+
+    /**
+     * Index
+     *
+     * @access  public
+     * @abstract
+     * @return  void
+     */
+    abstract public function index() : void;
+
+}
