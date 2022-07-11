68464a1bfcf0bcf9de2d0f9cb5aaa3b759984f5d
johninamillion <johninamio@gmail.com>
2022-07-11 12:26:36 +0200

    create an instance of the Register model

--- src/Controller/Register.php
+++ src/Controller/Register.php
@@ -3,9 +3,35 @@
 namespace SAE\PHPCMS\Controller;
 
 use SAE\PHPCMS\Controller;
+use SAE\PHPCMS\Model\Register as RegisterModel;
 
 final class Register extends Controller {
 
+    /**
+     * Register Model
+     *
+     * @access  private
+     * @var     RegisterModel|NULL
+     */
+    private ?RegisterModel $Model = NULL;
+
+    /**
+     * Construct
+     *
+     * @access  public
+     * @constructor
+     */
+    public function __construct() {
+        $this->Model = new RegisterModel();
+        parent::__construct();
+    }
+
+    /**
+     * Index
+     *
+     * @access  public
+     * @return  void
+     */
     public function index(): void {
         $this->View->setTitle( _( 'Registration' ) );
         $this->View->getTemplatePart( 'header' );
