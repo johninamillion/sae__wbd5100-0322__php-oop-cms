ab29a3095a826f06d9e5718b876708d6a726606f
johninamillion <johninamio@gmail.com>
2022-07-11 12:07:34 +0200

    set titles

--- src/Controller/Login.php
+++ src/Controller/Login.php
@@ -7,6 +7,7 @@ use SAE\PHPCMS\Controller;
 final class Login extends Controller {
 
     public function index(): void {
+        $this->View->setTitle( _( 'Login' ) );
         $this->View->getTemplatePart( 'header' );
         $this->View->getTemplatePart( 'login/index' );
         $this->View->getTemplatePart( 'footer' );
--- src/Controller/Register.php
+++ src/Controller/Register.php
@@ -7,6 +7,7 @@ use SAE\PHPCMS\Controller;
 final class Register extends Controller {
 
     public function index(): void {
+        $this->View->setTitle( _( 'Registration' ) );
         $this->View->getTemplatePart( 'header' );
         $this->View->getTemplatePart( 'register/index' );
         $this->View->getTemplatePart( 'footer' );
