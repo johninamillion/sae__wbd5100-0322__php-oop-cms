b67d5b927235a37885ec69fbd4f8afd18f228dc9
johninamillion <johninamio@gmail.com>
2022-07-11 11:29:18 +0200

    add database configuration

--- config.php
+++ config.php
@@ -6,3 +6,11 @@
 
 define( 'APP_ROOT_DIR',     __DIR__ );
 define( 'APP_PUBLIC_DIR',   __DIR__ . DIRECTORY_SEPARATOR . 'public' );
+
+define( 'DATABASE_TYPE',    'mysql' );
+define( 'DATABASE_HOST',    'localhost' );
+define( 'DATABASE_CHARSET', 'utf8' );
+define( 'DATABASE_PORT',    '3306' );
+define( 'DATABASE_NAME',    'sae_5100_0322_cms' );
+define( 'DATABASE_USER',    'root' );
+define( 'DATABASE_PASS',    'root' );
