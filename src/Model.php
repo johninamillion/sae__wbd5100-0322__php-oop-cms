019d891750946bc9d875e46342f94f1c06fd3b87
johninamillion <johninamio@gmail.com>
2022-07-12 10:32:17 +0200

    add functions for error handling

--- src/Model.php
+++ src/Model.php
@@ -5,6 +5,14 @@ namespace SAE\PHPCMS;
 abstract class Model {
 
     /**
+     * Errors
+     *
+     * @access  protected
+     * @var     array
+     */
+    protected array $errors = [];
+
+    /**
      * Database
      *
      * @access  protected
@@ -21,5 +29,20 @@ abstract class Model {
     public function __construct() {
         $this->Database = new Database();
     }
+
+    public function addError( ?string $key, ?string $message ) : void {
+        $this->errors[ $key ][] = $message;
+    }
+
+    public function getErrors( ?string $key = NULL ) : array {
+
+        return is_null( $key ) ? $this->errors : $this->errors[ $key ];
+    }
+
+    public function hasErrors( ?string $key ) : bool {
+
+        return isset( $this->errors[ $key ] ) && count( $this->errors[ $key ] ) > 0;
+    }
+
 
 }
