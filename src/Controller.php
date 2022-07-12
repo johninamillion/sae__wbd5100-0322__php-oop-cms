721eb50c70c4f845e4dd7ee135970cd1c0e1d574
johninamillion <johninamio@gmail.com>
2022-07-12 22:21:28 +0200

    add doc-block

--- src/Controller.php
+++ src/Controller.php
@@ -2,6 +2,15 @@
 
 namespace SAE\PHPCMS;
 
+/**
+ * Controller
+ *
+ * Controller is an abstract class contains all methods and properties that could be used in the controllers of this application.
+ * This class should be used as parent for all controllers in this application.
+ * This class should be used statically and should not be initialized.
+ *
+ * @abstract
+ */
 abstract class Controller {
 
     const METHOD_GET = 'GET';
@@ -16,6 +25,10 @@ abstract class Controller {
     protected ?View $View = NULL;
 
     /**
+     * Is Method
+     *
+     * Check if the request method equals the method passed as argument.
+     *
      * @access  protected
      * @param   string  $method
      * @return  bool
@@ -26,6 +39,10 @@ abstract class Controller {
     }
 
     /**
+     * Redirect
+     *
+     * Redirect the user to another location in the application.
+     *
      * @access  protected
      * @param   string  $location
      * @return  void
@@ -35,6 +52,10 @@ abstract class Controller {
     }
 
     /**
+     * Set Response Code
+     *
+     * Set a http response code.
+     *
      * @access  protected
      * @param   int     $code
      * @return  void
@@ -44,6 +65,8 @@ abstract class Controller {
     }
 
     /**
+     * Construct
+     *
      * @access  public
      * @constructor
      */
@@ -53,6 +76,9 @@ abstract class Controller {
 
     /**
      * Index
+     *
+     * Any child of this class should contain an index method returning void.
+     * This method should build a view for the controller.
      *
      * @access  public
      * @abstract
