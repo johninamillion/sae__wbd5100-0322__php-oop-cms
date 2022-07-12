41edd86a1b019d50fee17c2b643532e9d4e0c951
johninamillion <johninamio@gmail.com>
2022-07-12 22:26:38 +0200

    add doc-block

--- src/Model.php
+++ src/Model.php
@@ -2,6 +2,15 @@
 
 namespace SAE\PHPCMS;
 
+/**
+ * Model
+ *
+ * Controller is an abstract class contains all methods and properties that could be used in the models of this application.
+ * This class should be used as parent for all models in this application.
+ * This class should be used statically and should not be initialized.
+ *
+ * @abstract
+ */
 abstract class Model {
 
     /**
@@ -30,15 +39,37 @@ abstract class Model {
         $this->Database = new Database();
     }
 
+    /**
+     * Add Error
+     *
+     * @access  public
+     * @param   string|NULL $key
+     * @param   string|NULL $message
+     * @return  void
+     */
     public function addError( ?string $key, ?string $message ) : void {
         $this->errors[ $key ][] = $message;
     }
 
+    /**
+     * Get Errors
+     *
+     * @access  public
+     * @param   string|NULL $key
+     * @return  array
+     */
     public function getErrors( ?string $key = NULL ) : array {
 
         return is_null( $key ) ? $this->errors : $this->errors[ $key ];
     }
 
+    /**
+     * Has Errors
+     *
+     * @access  public
+     * @param   string|NULL $key
+     * @return  bool
+     */
     public function hasErrors( ?string $key ) : bool {
 
         return isset( $this->errors[ $key ] ) && count( $this->errors[ $key ] ) > 0;
