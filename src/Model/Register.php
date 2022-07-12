d7164a3dea54152098a413feb4ac1e7032fd0991
johninamillion <johninamio@gmail.com>
2022-07-12 11:20:35 +0200

    move functions to trait

--- src/Model/Register.php
+++ src/Model/Register.php
@@ -3,8 +3,11 @@
 namespace SAE\PHPCMS\Model;
 
 use SAE\PHPCMS\Model;
+use SAE\PHPCMS\Model\Traits\Users;
 
 final class Register extends Model {
+
+    use Users;
 
     /**
      * Create salt
@@ -34,37 +37,6 @@ final class Register extends Model {
         /** @var \PDOStatement $Statement */
         $Statement = $this->Database->prepare( $query );
         $Statement->bindValue( ':email', $email );
-        $Statement->execute();
-
-        return $Statement->rowCount() > 0;
-    }
-
-    /**
-     * Hash password
-     *
-     * @access  private
-     * @param   string  $password
-     * @param   string  $salt
-     * @return  string
-     */
-    private function hashPassword( string $password, string $salt ) : string {
-
-        return hash( 'sha512', "{$salt}{$password}" );
-    }
-
-    /**
-     * Check if username exists
-     *
-     * @access  private
-     * @param   string|NULL $username
-     * @return  bool
-     */
-    private function usernameExists( ?string $username ) : bool {
-        /** @var string $query */
-        $query = 'SELECT username FROM users WHERE username = :username;';
-        /** @var \PDOStatement $Statement */
-        $Statement = $this->Database->prepare( $query );
-        $Statement->bindValue( ':username', $username );
         $Statement->execute();
 
         return $Statement->rowCount() > 0;
