8343fea6eb88fdf9ef85f70084251ac6324fa887
johninamillion <johninamio@gmail.com>
2022-07-12 14:17:29 +0200

    add autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,14 @@
 
     'use strict';
 
-    const gulp = require( 'gulp' );
-    const sass = require( 'gulp-sass' )( require( 'sass' ) );
+    const gulp          = require( 'gulp' );
+    const sass          = require( 'gulp-sass' )( require( 'sass' ) );
+    const autoprefixer  = require( 'gulp-autoprefixer' );
 
     function compileSCSS() {
         return gulp.src( './public/assets/src/scss/**/*.scss' )
             .pipe( sass().on( 'error', sass.logError ) )
+            .pipe( autoprefixer() )
             .pipe( gulp.dest( './public/assets/dist/css' ) );
     }
 
