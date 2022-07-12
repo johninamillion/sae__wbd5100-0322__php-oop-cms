1aa193a3ced75c6ef3649d7b322aa2a309d2c6e2
johninamillion <johninamio@gmail.com>
2022-07-12 11:27:00 +0200

    login user on submitted form

--- src/Controller/Login.php
+++ src/Controller/Login.php
@@ -3,10 +3,47 @@
 namespace SAE\PHPCMS\Controller;
 
 use SAE\PHPCMS\Controller;
+use SAE\PHPCMS\Model\Login as LoginModel;
 
 final class Login extends Controller {
 
+    /**
+     * Login Model
+     *
+     * @access  private
+     * @var     LoginModel|NULL
+     */
+    private ?LoginModel $Model = NULL;
+
+    /**
+     * Construct
+     *
+     * @access  public
+     * @constructor
+     */
+    public function __construct() {
+        $this->Model = new LoginModel();
+        parent::__construct();
+    }
+
+    /**
+     * Index
+     *
+     * @access  public
+     * @return  void
+     */
     public function index(): void {
+        if ( $this->isMethod( self::METHOD_POST ) ) {
+            /** @var string|NULL $username */
+            $username = filter_input( INPUT_POST, 'username' );
+            /** @var string|NULL $password */
+            $password = filter_input( INPUT_POST, 'password' );
+
+            if ( $this->Model->login( $username, $password ) ) {
+                $this->redirect( '/' );
+            }
+        }
+
         $this->View->setTitle( _( 'Login' ) );
         $this->View->getTemplatePart( 'header' );
         $this->View->getTemplatePart( 'login/index' );
