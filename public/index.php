f1e3ffb5973e80731893ea32a35b3ec78c37de2a
johninamillion <johninamio@gmail.com>
2022-07-12 20:38:04 +0200

    update comment

--- public/index.php
+++ public/index.php
@@ -4,6 +4,9 @@
  * Index File
  *
  * This is the entry point for the application.
+ *
+ * The document root of your apache configuration should point in this directory.
+ * 'mod_rewrite' should be enabled for a correct interpretation of the .htaccess file.
  */
 
 namespace SAE\PHPCMS;
